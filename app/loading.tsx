2f931f931162e5b312350de12008c9b403a3b5e5
fiezt <dat09t@gmail.com>
2023-07-19 17:07:19 +0700

    beautify loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,10 @@
+import { Loader } from 'lucide-react'
 import React from 'react'
 
 function loading() {
     return (
-        <div className="h-full flex items-center justify-center">
-            Loading...
+        <div className="h-screen flex items-center justify-center">
+            <Loader className="mr-2 h-4 w-4 animate-spin" /> Loading...
         </div>
     )
 }
